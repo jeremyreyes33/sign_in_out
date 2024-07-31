a39b29cb4bb25acd2d5dcdc24fbdf36872959d1d
JeremyReyes33 <138751066+jeremyreyes33@users.noreply.github.com>
2024-07-31 15:41:33 +0800

    Add files via upload

---
+++ Sign_in_out/sign_inout.php
@@ -0,0 +1,133 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sign in || Sign up from</title>
+     <!-- font awesome icons -->
+     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+     <!-- Bootstrap CDN -->
+     <!-- <link rel="stylesheet" href=" https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css"> -->
+    
+    <!-- css stylesheet -->
+    <link rel="stylesheet" href="../css/style.css">
+</head>
+<body>
+    <!-- Header -->
+    <header class="container-fluid">
+        <h5 id="kuyaje">KuyaJe Shop</h5>
+        <nav>
+            <ul>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">About</a></li>
+                <li><a href="#">Login</a></li>
+            </ul>
+        </nav>
+        <a href="">Contact</a>
+    </header>
+<!-- Container -->
+    <div class="container" id="container">
+        <div class="form-container sign-up-container">
+            <form action="#">
+                <h1>Create Account</h1>
+                <div class="social-container">
+                    <a href="#" class="social"><i class="fab fa-facebook-f"></i></a>
+                    <a href="#" class="social"><i class="fab fa-google-plus-g"></i></a>
+                    <a href="#" class="social"><i class="fab fa-linkedin-in"></i></a>
+                </div>
+                <span>or use your email for registration</span>
+                <div class="infield">
+                    <input type="text" placeholder="Name" />
+                    <label></label>
+                </div>
+                <div class="infield">
+                    <input type="email" placeholder="Email" name="email"/>
+                    <label></label>
+                </div>
+                <div class="infield">
+                    <input type="password" placeholder="Password" />
+                    <label></label>
+                </div>
+                <button>Sign Up</button>
+            </form>
+        </div>
+        <div class="form-container sign-in-container">
+            <form action="#">
+                <h1>Sign in</h1>
+                <div class="social-container">
+                    <a href="#" class="social"><i class="fab fa-facebook-f"></i></a>
+                    <a href="#" class="social"><i class="fab fa-google-plus-g"></i></a>
+                    <a href="#" class="social"><i class="fab fa-linkedin-in"></i></a>
+                </div>
+                <span>or use your account</span>
+                <div class="infield">
+                    <input type="email" placeholder="Email" name="email"/>
+                    <label></label>
+                </div>
+                <div class="infield">
+                    <input type="password" placeholder="Password" />
+                    <label></label>
+                </div>
+                <a href="#" class="forgot">Forgot your password?</a>
+                <button>Sign In</button>
+            </form>
+        </div>
+        <div class="overlay-container" id="overlayCon">
+            <div class="overlay">
+                <div class="overlay-panel overlay-left">
+                    <h1>Welcome Back!</h1>
+                    <p>To keep connected with us please login with your personal info</p>
+                    <button>Sign In</button>
+                </div>
+                <div class="overlay-panel overlay-right">
+                    <h1>Hello, Friend!</h1>
+                    <p>Enter your personal details and start journey with us</p>
+                    <button>Sign Up</button>
+                </div>
+            </div>
+            <button id="overlayBtn"></button>
+        </div>
+    </div>
+    <!-- End of Container -->
+    <footer>
+        <mark>See more on <a href="">Kuya Je Shop</a></mark>
+    </footer>
+    
+    <!-- js code -->
+    <script>
+            const container = document.getElementById('container');
+            const overlayCon = document.getElementById('overlayCon');
+            const overlayBtn = document.getElementById('overlayBtn');
+            
+                let kuyaje = document.getElementById('kuyaje');
+
+                kuyaje.addEventListener("mouseover", () => {
+                    if(kuyaje.innerText === "KuyaJe Shop"){
+                        kuyaje.style = "transition: all 2s ease-in-out"; 
+                        kuyaje.style = "color: #c4c59";
+                        kuyaje.innerText = "Jeremy Shop";
+                    }
+                });
+
+                kuyaje.addEventListener("mouseout", () => {
+                    if(kuyaje.innerText === "Jeremy Shop"){
+                        kuyaje.style = "transition: 2s ease"; 
+                        kuyaje.style= "color: #ddd";
+                        kuyaje.innerText = "KuyaJe Shop";
+                    }
+                });
+
+            // container.style = "left:22.5%; top:1.5%";
+
+            overlayBtn.addEventListener('click', () => {
+                container.classList.toggle('right-panel-active');
+                overlayBtn.classList.remove('btnScaled');
+                window.requestAnimationFrame(()=>{
+                    overlayBtn.classList.add('btnScaled');
+                });
+            });
+    </script>
+
+</body>
+</html>
